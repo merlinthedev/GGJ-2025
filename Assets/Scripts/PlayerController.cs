8c0a7bef57d1eeab2553d3633d5593171fffc863
merlin <merlijnvan11@gmail.com>
2025-01-25 12:11:49 +0100

    jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,8 @@ public class PlayerController : MonoBehaviour
     [Header("Movement Settings")] [SerializeField]
     private float movementSpeed = 5f;
 
+    [SerializeField] private float jumpSpeed = 5f;
+
     [Header("Look Settings")] [SerializeField]
     private float sensitivity = 5f;
 
@@ -17,10 +19,17 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] private Rigidbody capsuleRigidbody;
 
+    [Header("Ground Check")] [SerializeField]
+    private Transform groundCheck;
+
+    private float groundCheckRadius = 0.4f;
+
     private Vector2 moveInput = Vector2.zero;
     private Vector2 lookInput = Vector2.zero;
     private float pitch = 0f;
 
+    private bool isGrounded = false;
+    private bool jumpPressed = false;
 
     private void Start()
     {
@@ -30,6 +39,12 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         HandleMovement();
+    }
+
+    private void Update()
+    {
+        CheckGround();
+        HandleJump();
     }
 
     private void LateUpdate()
@@ -60,6 +75,22 @@ public class PlayerController : MonoBehaviour
         cameraTransform.localEulerAngles = new Vector3(pitch, 0f, 0f);
     }
 
+    private void HandleJump()
+    {
+        if (jumpPressed && isGrounded)
+        {
+            capsuleRigidbody.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+        }
+
+        jumpPressed = false;
+    }
+
+    private void CheckGround()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundCheckRadius, LayerMask.GetMask("Ground"),
+            QueryTriggerInteraction.Ignore);
+    }
+
     public void OnMove(InputAction.CallbackContext context)
     {
         moveInput = context.ReadValue<Vector2>();
@@ -68,5 +99,22 @@ public class PlayerController : MonoBehaviour
     public void OnLook(InputAction.CallbackContext context)
     {
         lookInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed)
+        {
+            jumpPressed = true;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (groundCheck != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+        }
     }
 }
